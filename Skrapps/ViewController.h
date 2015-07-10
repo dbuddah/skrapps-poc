4ac87615df7d0b1bed59245095119f316bf1ff7c
Doug McCoy <dmccoy@gracenote.com>
2015-07-09 19:56:32 -0700

    Initial Commit

---
+++ Skrapps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Skrapps
+//
+//  Created by Doug McCoy on 7/9/15.
+//  Copyright (c) 2015 skrapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
