8fbfe26ab5950d4ffc22f99d1d34d2434634d074
Doug McCoy <dmccoy@gracenote.com>
2015-07-10 14:06:16 -0700

    stub service manager for Skrapps service

---
+++ Skrapps/ServiceManager/SkrappsServiceManager.h
@@ -0,0 +1,13 @@
+//
+//  SkrappsServiceMnager.h
+//  Skrapps
+//
+//  Created by Doug McCoy on 7/10/15.
+//  Copyright (c) 2015 skrapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SkrappsServiceManager : NSObject
+
+@end
---
+++ Skrapps/ServiceManager/SkrappsServiceManager.m
@@ -0,0 +1,13 @@
+//
+//  SkrappsServiceMnager.m
+//  Skrapps
+//
+//  Created by Doug McCoy on 7/10/15.
+//  Copyright (c) 2015 skrapps. All rights reserved.
+//
+
+#import "SkrappsServiceManager.h"
+
+@implementation SkrappsServiceManager
+
+@end
